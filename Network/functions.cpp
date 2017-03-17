271fa20d5e2fbb9770b42e6bf409a6e59d4ef358
Garrett Swann <garrettswann3049@yahoo.com>
2017-03-16 18:20:18 -0700

    Working on breed function. Skeleton more or less down.

--- Network/functions.cpp
+++ Network/functions.cpp
@@ -14,7 +14,27 @@ long double random_weight()
 
 BRAIN_T breed(BRAIN_T &mom,BRAIN_T &dad, vector<int> &nodes_per_layer)
 {
-    return mom;
+    BRAIN_T child;
+    child.resize(nodes_per_layer.size());
+    for(int cur_layer=0; cur_layer < nodes_per_layer.size()-1; cur_layer++)
+    {
+        child[cur_layer].resize( nodes_per_layer[cur_layer] );
+        for( int next_layer=0; next_layer < nodes_per_layer[cur_layer]; next_layer++)
+        {
+            child[cur_layer].resize( nodes_per_layer[cur_layer] );
+
+            //TODO Right about here
+        }
+    }
+
+    for(int cur_layer=0; cur_layer < nodes_per_layer.size(); cur_layer++)
+    {
+        int splice_place = rand() % nodes_per_layer[cur_layer];
+        for(int i=0; i<nodes_per_layer[cur_layer]; i++)
+        {
+            child.
+        }
+    }
 }
 
 BRAIN_T splice(BRAIN_T &mom,BRAIN_T &dad, vector<int> &nodes_per_layer)
